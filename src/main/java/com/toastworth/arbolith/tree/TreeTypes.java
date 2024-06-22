e3e73b89450341f2a25bb3cfd815b70ff6af9df0
ToadsworthLP <fiery.toadsworth@gmail.com>
2024-06-22 21:20:03 +0200

    Replaced aspen with silverwood and finished larch

--- src/main/java/com/toastworth/arbolith/tree/TreeTypes.java
+++ src/main/java/com/toastworth/arbolith/tree/TreeTypes.java
@@ -2,24 +2,26 @@ package com.toastworth.arbolith.tree;
 
 import com.google.common.collect.ImmutableList;
 import com.toastworth.arbolith.wood.WoodSets;
+import net.minecraft.util.valueproviders.BiasedToBottomInt;
 import net.minecraft.util.valueproviders.ClampedNormalInt;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.UniformInt;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FancyFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.MegaPineFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.SpruceFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.treedecorators.BeehiveDecorator;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.FancyTrunkPlacer;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.ForkingTrunkPlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraftforge.registries.DeferredRegister;
 
-import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalInt;
@@ -81,31 +83,47 @@ public class TreeTypes {
                     .decorators(hasBees ? ImmutableList.of(new BeehiveDecorator(0.05f)) : ImmutableList.of()).build()),
             1);
 
-    public static final TreeType ASPEN_TREE_TYPE = new TreeType("aspen", "Aspen") // TODO find Terralith biome to put this in - maybe ditch yellow and make it silverwood?
+    public static final TreeType SILVERWOOD_TREE_TYPE = new TreeType("silverwood", "Silverwood")
             .withConfiguration("normal", ((leavesBlock, hasBees) -> new TreeConfiguration.TreeConfigurationBuilder(
-                    BlockStateProvider.simple(WoodSets.ASPEN_SET.getLogBlock().get()),
-                    new StraightTrunkPlacer(7, 1, 1),
+                    BlockStateProvider.simple(WoodSets.SILVERWOOD_SET.getLogBlock().get()),
+                    new ForkingTrunkPlacer(5, 2, 2),
                     BlockStateProvider.simple(leavesBlock.get()),
-                    new FancyFoliagePlacer(ConstantInt.of(1), ConstantInt.of(0), 6),
-                    new TwoLayersFeatureSize(1, 0, 1))
+                    new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(2), 3),
+                    new TwoLayersFeatureSize(0, 0, 0, OptionalInt.of(16)))
                     .decorators(hasBees ? ImmutableList.of(new BeehiveDecorator(0.05f)) : ImmutableList.of()).build()),
-            9);
-
-    public static final TreeType LARCH_TREE_TYPE = new TreeType("larch", "Larch") // TODO set up proper tree shape + add as "lark" to Terralith
+                9)
+            .withConfiguration("large", ((leavesBlock, hasBees) -> new TreeConfiguration.TreeConfigurationBuilder(
+                    BlockStateProvider.simple(WoodSets.SILVERWOOD_SET.getLogBlock().get()),
+                    new FancyTrunkPlacer(10, 6, 10),
+                    BlockStateProvider.simple(leavesBlock.get()),
+                    new FancyFoliagePlacer(ConstantInt.of(4), ConstantInt.of(2), 3),
+                    new TwoLayersFeatureSize(0, 0, 0, OptionalInt.of(16)))
+                    .decorators(hasBees ? ImmutableList.of(new BeehiveDecorator(0.05f)) : ImmutableList.of()).build()),
+                1);
+    public static final TreeType LARCH_TREE_TYPE = new TreeType("larch", "Larch")
             .withConfiguration("normal", ((leavesBlock, hasBees) -> new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(WoodSets.LARCH_SET.getLogBlock().get()),
-                    new StraightTrunkPlacer(7, 1, 1),
+                    new StraightTrunkPlacer(5, 2, 1),
                     BlockStateProvider.simple(leavesBlock.get()),
-                    new FancyFoliagePlacer(ConstantInt.of(1), ConstantInt.of(0), 6),
+                    new SpruceFoliagePlacer(UniformInt.of(2, 3), UniformInt.of(0, 2), UniformInt.of(1, 2)),
+                    new TwoLayersFeatureSize(2, 0, 2))
+                    .ignoreVines()
+                    .decorators(hasBees ? ImmutableList.of(new BeehiveDecorator(0.05f)) : ImmutableList.of()).build()),
+                    9)
+            .withConfiguration("large", ((leavesBlock, hasBees) -> new TreeConfiguration.TreeConfigurationBuilder(
+                    BlockStateProvider.simple(WoodSets.LARCH_SET.getLogBlock().get()),
+                    new StraightTrunkPlacer(7, 4, 10),
+                    BlockStateProvider.simple(leavesBlock.get()),
+                    new MegaPineFoliagePlacer(ConstantInt.of(1), UniformInt.of(1, 3), UniformInt.of(12, 18)),
                     new TwoLayersFeatureSize(1, 0, 1))
                     .decorators(hasBees ? ImmutableList.of(new BeehiveDecorator(0.05f)) : ImmutableList.of()).build()),
-            9);
+                    1);
 
     static {
         TREE_TYPES.add(CHERRY_TREE_TYPE);
         TREE_TYPES.add(RED_MAPLE_TREE_TYPE);
         TREE_TYPES.add(ORANGE_MAPLE_TREE_TYPE);
-        TREE_TYPES.add(ASPEN_TREE_TYPE);
+        TREE_TYPES.add(SILVERWOOD_TREE_TYPE);
         TREE_TYPES.add(LARCH_TREE_TYPE);
     }
 
